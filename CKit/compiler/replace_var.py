ce89f1ab5e0e9d6144f5e15e2f4726adc7b8c51e
MasterMattK <103427018+MasterMattK@users.noreply.github.com>
2023-12-14 15:54:18 -0600

    Made replace_var.py paths relative

--- CKit/compiler/replace_var.py
+++ CKit/compiler/replace_var.py
@@ -1,5 +1,5 @@
-mapDir = "D:\\EVERYTHING\\Projects\\Multiplayer\\SMSO CKit\\compiler\\project.map"   # specify which directory the .map file is
-scriptDir = "C:\\SMSO Dev\\Source\\Client\\InGameVars.py"   # specify which file the vars should be stored in. make sure it's a .py file so you can import it
+mapDir = ".\\project.map"   # specify which directory the .map file is
+scriptDir = "..\\..\\GUI\\Client\\InGameVars.py"   # specify which file the vars should be stored in. make sure it's a .py file so you can import it
 varsToGet = [("gamemode", "GAMEMODE"), ("tagBool", "TAG_BOOL"), ("isTagger", "IS_TAGGER"), ("clientTagTime", "CLIENT_TAG_TIME"),  
 	("connected", "CONNECTED"), ("marios", "MARIOS"), ("bmdPointers", "BMD_POINTERS"), ("modelIndex", "MODEL_INDEX"), ("musicVolume", "MUSIC_VOLUME"), 
 	("fluddRefills", "FLUDD_REFILLS"), ("currentTime", "CURRENT_TIME"), ("flagToUpdate", "FLAG_TO_UPDATE"), ("bResetFlags", "RESET_FLAGS"),
